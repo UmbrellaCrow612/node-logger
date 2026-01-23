f4dd7159a33b7c4353ee9d8954e00a34f47cec4f
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-23 10:51:37 +0000

    works

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,15 @@
 import src = require("../src");
 
-let logger = new src.NodeLogger();
+let logger = new src.NodeLogger({
+  showLogTime: false,
+  logFileRetentionPeriodInDays: 30,
+  logFilesBasePath: "./logs",
+  saveToLogFile: true,
+  useColoredOutput: true,
+});
 
 logger.info("Hello world");
 logger.warn("Hello world");
 logger.error(new Error(""));
+
+logger.destroy();
