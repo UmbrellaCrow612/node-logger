afb9b140d48644711ee37342cbcfe64de8c4d99b
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-24 18:06:31 +0000

    Update node_process.ts

--- src/node_process.ts
+++ src/node_process.ts
@@ -8,6 +8,15 @@ import type types = require("./types");
 import fs = require("fs");
 import path = require("path");
 
+/**
+ * Gets todays lof file path in format YYYY-MM-DD and returns a string for example `c:/dev/logs/2026-01.23.log`
+ */
+function getTodaysLogFile(options: types.NodeProcessOptions): string {
+  const today = new Date().toISOString().split("T")[0];
+  const filePath = path.join(options.basePath, `${today}.log`);
+  return filePath;
+}
+
 async function main() {
   /**
    * Holds args passed to the CLI
@@ -45,17 +54,13 @@ async function main() {
       fs.mkdirSync(options.basePath);
       console.log("base dir " + options.basePath + " created");
     }
-
-    let info = await fs.promises.stat(options.basePath);
-
-    if (!info.isDirectory()) {
-      throw new Error("Base path " + options.basePath + " cannot be a file");
-    }
   } catch (error) {
     console.error("Failed to make base dir");
     console.error(error);
     process.exit(1);
   }
+
+  console.log(getTodaysLogFile(options))
 
   /**
    * Holds the stdin buffer i.e content written to the process
