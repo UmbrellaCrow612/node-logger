009d2a65d9ecef2eb469826a6a7b05caa6283d9d
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-24 11:28:17 +0000

    Update stdin.go

--- cli/commands/stdin.go
+++ cli/commands/stdin.go
@@ -3,18 +3,44 @@ package commands
 import (
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/UmbrellaCrow612/node-logger/cli/console"
 	"github.com/UmbrellaCrow612/node-logger/cli/logfiles"
 	"github.com/UmbrellaCrow612/node-logger/cli/t"
 )
 
-// List of command and what sneed for them to run from the cli stdin
+var (
+	logFilePath string
+	logWriter   *os.File
+	once        sync.Once
+)
+
+func initLogWriter(options *t.ArgOptions) {
+	fp, err := logfiles.GetTodaysLogFile(options)
+	if err != nil {
+		console.ExitWithError(err)
+	}
+
+	file, err := os.OpenFile(fp, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		console.ExitWithError(err)
+	}
+
+	logFilePath = fp
+	logWriter = file
+}
+
 var CommandActions = []t.CommandAndAction{
 	{
 		PrefixMatcher: "exit",
 		Action: func(options *t.ArgOptions, line string) error {
 			console.Info("Exiting...")
+
+			if logWriter != nil {
+				_ = logWriter.Close()
+			}
+
 			os.Exit(0)
 			return nil
 		},
@@ -22,20 +48,19 @@ var CommandActions = []t.CommandAndAction{
 	{
 		PrefixMatcher: "write:",
 		Action: func(options *t.ArgOptions, line string) error {
-			fp, err := logfiles.GetTodaysLogFile(options)
-			if err != nil {
-				console.ExitWithError(err)
+			once.Do(func() {
+				initLogWriter(options)
+				console.Info("Once")
+			})
+
+			if logWriter == nil {
+				return nil
 			}
 
-			file, err := os.OpenFile(fp, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				console.ExitWithError(err)
-			}
-			defer file.Close()
-
-			if _, err := file.WriteString(strings.TrimPrefix(line, "write:") + "\n"); err != nil {
+			if _, err := logWriter.WriteString(strings.TrimPrefix(line, "write:") + "\n"); err != nil {
 				return err
 			}
+
 			return nil
 		},
 	},
